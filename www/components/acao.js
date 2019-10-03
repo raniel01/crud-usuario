e14d2db3462e06a07b848c9022170ff55e2f5024
raniel01 <ranielsnts@hotmail.com>
2019-10-03 09:35:51 +0900

    iniciando projeto

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,4 +1,28 @@
 // This is a JavaScript file
 
- 
+ $(document).on("click", "#salvar", function(){
+   var parametros = {
+     "nome" : $("#nome").val(),
+     "senha" : $("#senha").val(),
+     "email" : $("#email").val(),
+   };
+   $.ajax({
+     type:"post", // como enviar
+     url:""
+     data:parametros, // o que quer 
+
+     //se der certo 
+     sucess: function(data){
+       navigator.notification.alert(data);
+       $("#nome").val("");
+       $("#senha").val("");
+       $("#email").val("");
+
+     },
+     //se der errado
+     error: function(data){
+       navigator.notification.alert("Erro ao cadastrar!");
+     }
+   });
+ });
         
